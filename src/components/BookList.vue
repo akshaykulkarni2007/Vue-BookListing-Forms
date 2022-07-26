38c21d6b49baf5a70f6130c1dc04eb1b2da35271
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2022-07-26 22:15:55 +0530

    module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" placeholder="Search Books" v-model="searchInput"/>
+
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
 
     <br><hr>
@@ -59,12 +61,19 @@ export default {
         }
       ],
       filters: ["bought", "borrowed"],
-      holding: "bought"
+      holding: "bought",
+      searchInput: ""
     };
   },
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
